f9b1d0aea42a76a2f8952d1e44c8c5a9260b2f3e
Phani <33480024+K-Phaneendra@users.noreply.github.com>
2019-04-14 16:22:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// Import express
+import http from 'http';
+import express from './services/express';
+import config from './config';
+import apiRoutes from './src/api-routes';
+
+// Import Mongoose
+const mongoose = require('mongoose');
+
+// Initialize the app
+const app = express(apiRoutes);
+const server = http.createServer(app);
+
+const port = config.port;
+const mongoUri = config.mongo.uri;
+
+// Connect to Mongoose and set connection variable
+mongoose.connect(mongoUri);
+
+// Send message for default URL
+app.get('/', (req, res) =>
+  res.send(`Node with Express is running on port ${port}`)
+);
+
+// Launch app to listen to specified port
+server.listen(port, function() {
+  console.log('Running api on port ' + port);
+});
